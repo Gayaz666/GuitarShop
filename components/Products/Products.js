5b59a721ea22c554f305c96897166fe368f93a8c
Gayaz Urazov <gayazur@mail.ru>
2025-10-28 16:47:42 +0500

    6 lesson of let's code channel

---
+++ components/Products/Products.js
@@ -0,0 +1,31 @@
+class Products {
+
+  render() {
+    let htmlCatalog = '';
+
+
+    CATALOG.forEach(({ id, name, price, img }) => {
+      htmlCatalog += `
+        <li class="products-element">
+          <span class="products-element__name">${name}</span>
+          <img src=${img} class="products-element__img">
+          <span class="products-element__price">
+            😏  ${price.toLocaleString()} USD
+          </span>
+          <button class="products-element__btn">Добавить в корзину</button>
+        </li>
+      `;
+    });
+
+    const html = `
+      <ul class="products-container">
+        ${htmlCatalog}
+      </ul>
+    `;
+
+    ROOT_PRODUCTS.innerHTML = html;
+  }
+}
+
+const productsPage = new Products();
+productsPage.render();
---
+++ constants/catalog.js
@@ -0,0 +1,62 @@
+const CATALOG = [
+    {
+        "id": "el1",
+        "name": "FENDER SQUIER BULLET STRAT HT HSS BLK",
+        "img": "https://i.ibb.co/m5RDTpNW/1.webp",
+        "price": 13600
+    },
+    {
+        "id": "el2",
+        "name": "FENDER SQUIER AFFINITY",
+        "img": "https://i.ibb.co/Wp21rMKK/2.webp",
+        "price": 23900
+    },
+    {
+        "id": "el3",
+        "name": "IBANEZ GIO GRG121DX-BKF BLACK FLAT",
+        "img": "https://i.ibb.co/S740XM0g/3.webp",
+        "price": 21600
+    },
+    {
+        "id": "el4",
+        "name": "EPIPHONE LES PAUL STUDIO LT EBONY",
+        "img": "https://i.ibb.co/N6khssfw/4.webp",
+        "price": 19900
+    },
+    {
+        "id": "el5",
+        "name": "FENDER SQUIER BULLET TREM HSS AWT",
+        "img": "https://i.ibb.co/mn31Z0f/5.webp",
+        "price": 14500
+    },
+    {
+        "id": "el6",
+        "name": "FENDER SQUIER BULLET MUSTANG HH BLK",
+        "img": "https://i.ibb.co/wNBQTncj/6.webp",
+        "price": 14600
+    },
+    {
+        "id": "el7",
+        "name": "FENDER SQUIER BULLET STRAT HT AWT",
+        "img": "https://i.ibb.co/s95hgM9W/7.webp",
+        "price": 13600
+    },
+    {
+        "id": "el8",
+        "name": "JACKSON JS22-7 DINKY SATIN BLACK",
+        "img": "https://i.ibb.co/Tx65wKNF/8.webp",
+        "price": 28700
+    },
+    {
+        "id": "el9",
+        "name": "JACKSON JS11 DINKY OLYMPIC WHITE",
+        "img": "https://i.ibb.co/Wvb39xBH/9.webp",
+        "price": 18100
+    },
+    {
+        "id": "el10",
+        "name": "YAMAHA PACIFICA 012 WH",
+        "img": "https://i.ibb.co/8LpSpWS0/10.webp",
+        "price": 15990
+    }
+];
